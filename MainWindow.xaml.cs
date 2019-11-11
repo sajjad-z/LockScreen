ae86d175376311a0cd9bbca2bb97251cf04abd1e
sajjad <assassinsajjad0@gmail.com>
2019-11-11 18:21:28 +0330

    first buid

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace LockScreen
+{
+    /// <summary>
+    /// logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InstallMeOnStartUp();
+
+            // set width & height for App
+            int width = 0, height = 0;
+            foreach (var screen in Screen.AllScreens)
+            {
+                width += screen.WorkingArea.Width;
+                height += screen.WorkingArea.Height;
+            }
+            this.Width = width;
+            this.Height = height;
+
+            // hook keyboard
+            IntPtr hModule = GetModuleHandle(IntPtr.Zero);
+            hookProc = new LowLevelKeyboardProcDelegate(LowLevelKeyboardProc);
+            hHook = SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, hModule, 0);
+            DisableTaskManager();
+            //if (hHook == IntPtr.Zero)
+            //{
+            //    Console.WriteLine("Failed to set hook, error = " + Marshal.GetLastWin32Error());
+            //}
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (FloatingPasswordBox.Password != "@dmin")
+            {
+                ErrorGrid.Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb(244, 67, 54));
+                ErrorMessage.Text = "رمز عبور نامعتبر است";
+            }
+            else
+            {
+                ErrorGrid.Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb(76, 175, 80));
+                ErrorMessage.Text = "با موفقیت وارد می شوید";
+                System.Windows.Application.Current.Shutdown();
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            UnhookWindowsHookEx(hHook); // release keyboard hook
+            EnableCTRLALTDEL();
+        }
+
+        //private void Window_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        //{
+        //    try
+        //    {
+        //        if (Keyboard.Modifiers == ModifierKeys.Alt && e.SystemKey == Key.F4 ||
+        //          Keyboard.Modifiers == ModifierKeys.Control && e.SystemKey == Key.Escape ||
+        //                Keyboard.Modifiers == ModifierKeys.Alt && e.SystemKey == Key.Tab)
+        //        {
+        //            e.Handled = true;
+        //        }
+
+        //        else if (Keyboard.Modifiers == ModifierKeys.Windows)
+        //        {
+        //            e.Handled = true;
+        //        }
+        //        else
+        //        {
+        //            base.OnPreviewKeyDown(e);
+        //        }
+        //    }
+        //    catch { }
+        //}
+
+        void InstallMeOnStartUp()
+        {
+            try
+            {
+                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                Assembly curAssembly = Assembly.GetExecutingAssembly();
+                if (key.GetValue(curAssembly.GetName().Name) == null)
+                    key.SetValue(curAssembly.GetName().Name, curAssembly.Location);
+            }
+            catch { }
+        }
+
+        private void DisableTaskManager()
+        {
+            RegistryKey regkey = default(RegistryKey);
+            string keyValueInt = "1";
+            string subKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+            try
+            {
+                regkey = Registry.CurrentUser.CreateSubKey(subKey);
+                regkey.SetValue("DisableTaskMgr", keyValueInt);
+                regkey.Close();
+            }
+            catch (Exception ex)
+            {
+                //System.Windows.MessageBox.Show("Error " + ex.Message);
+            }
+        }
+
+        public static void EnableCTRLALTDEL()
+        {
+            try
+            {
+                string subKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+                RegistryKey rk = Registry.CurrentUser;
+                RegistryKey sk1 = rk.OpenSubKey(subKey);
+                if (sk1 != null)
+                    rk.DeleteSubKeyTree(subKey);
+            }
+            catch { }
+        }
+
+        // Disable Real All Key out of Program (Except ctrl+alt+delete)
+        private struct KBDLLHOOKSTRUCT
+        {
+            public int vkCode;
+            int scanCode;
+            public int flags;
+            int time;
+            int dwExtraInfo;
+        }
+
+        private delegate int LowLevelKeyboardProcDelegate(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProcDelegate lpfn, IntPtr hMod, int dwThreadId);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnhookWindowsHookEx(IntPtr hHook);
+
+        [DllImport("user32.dll")]
+        private static extern int CallNextHookEx(int hHook, int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(IntPtr path);
+
+        private IntPtr hHook;
+        LowLevelKeyboardProcDelegate hookProc; // prevent gc
+        const int WH_KEYBOARD_LL = 13;
+
+        private static int LowLevelKeyboardProc(int nCode, int wParam, ref KBDLLHOOKSTRUCT lParam)
+        {
+            if (nCode >= 0)
+                switch (wParam)
+                {
+                    case 256: // WM_KEYDOWN
+                    case 257: // WM_KEYUP
+                    case 260: // WM_SYSKEYDOWN
+                    case 261: // M_SYSKEYUP
+                        if (
+                            (lParam.vkCode == 0x09 && lParam.flags == 32) || // Alt+Tab
+                            (lParam.vkCode == 0x1b && lParam.flags == 32) || // Alt+Esc
+                            (lParam.vkCode == 0x73 && lParam.flags == 32) || // Alt+F4
+                            (lParam.vkCode == 0x1b && lParam.flags == 0) || // Ctrl+Esc
+                            (lParam.vkCode == 0x5b && lParam.flags == 1) || // Left Windows Key 
+                            (lParam.vkCode == 0x5c && lParam.flags == 1))    // Right Windows Key 
+                        {
+                            return 1; //Do not handle key events
+                        }
+                        break;
+                }
+            return CallNextHookEx(0, nCode, wParam, ref lParam);
+        }
+
+    }
+}
